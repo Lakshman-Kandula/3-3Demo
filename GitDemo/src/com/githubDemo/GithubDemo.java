7d9b58a7108b7323793e6ec8ff1c4c19a8b4ebb9
LAKSHMAN <kandula.lakshman18@gmail.com>
2023-03-03 08:50:53 +0530

    First commit via eclipse

---
+++ GitDemo/src/com/githubDemo/GithubDemo.java
@@ -0,0 +1,10 @@
+package com.githubDemo;
+import java.util.*;
+
+public class GithubDemo {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Good morning git.....");
+	}
+}
---
+++ GitDemo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author LAKSHMAN
+ *
+ */
+module GitDemo {
+}
