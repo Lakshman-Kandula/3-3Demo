6087479ceabba168b91fa46a68dbe8f41eaf8315
LAKSHMAN <kandula.lakshman18@gmail.com>
2023-03-03 09:18:01 +0530

    first commit

--- GitDemo/src/com/githubDemo/testgit.java
+++ GitDemo/src/com/githubDemo/testgit.java
@@ -1,11 +1,13 @@
 package com.githubDemo;
+import java.util.*;
 
 public class testgit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+		Scanner sc = new Scanner(System.in);
+		System.err.println("hi git.....");
 	}
 
 }
